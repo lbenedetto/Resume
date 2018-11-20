2fb52234f59c7012df49649299022722e6881c63
lbenedetto <larsbenedetto@toogoodtogo.com>
2018-11-20 19:24:45 +0100

    Add export script

---
+++ export.sh
@@ -0,0 +1,5 @@
+sudo hackmyresume build resume.json TO ./generated/resume.all -t ~/node_modules/jsonresume-theme-kendall
+rm ./generated/resume.pdf.html
+rm ./generated/resume.png.html
+rm ./generated/resume.yml
+rm ./generated/resume.json
